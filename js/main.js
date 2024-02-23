e0729df29298e05c677cf680ebd8206de2972301
YosvanySS <113038567+YosvanySS@users.noreply.github.com>
2024-02-24 00:51:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "interface" ? 50 : 50;
+camera.position.z = objToRender === "interface" ? 1 : 1;
 
 //Add lights to the scene, so we can actually see the 3D model
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
